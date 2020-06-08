8dc8d0e5a8388bdd258d116658bae62bd38ca494
andywang425 <56548456+andywang425@users.noreply.github.com>
2020-06-08 22:59:03 +0800

    Update B站直播自动抢辣条.user.js

--- B站直播自动抢辣条.user.js
+++ B站直播自动抢辣条.user.js
@@ -5,8 +5,8 @@
 // @author        andywang425
 // @description   自动参与Bilibili直播区广播礼物及小时榜房间礼物的抽奖;完成每日任务
 // @description:en 自动参与Bilibili直播区广播礼物及小时榜房间礼物的抽奖;完成每日任务
-// @updateURL     https://raw.githubusercontent.com/andywang425/Bilibili-LRHH/master/B站直播自动抢辣条.user.js
-// @downloadUR    https://raw.githubusercontent.com/andywang425/Bilibili-LRHH/master/B站直播自动抢辣条.user.js
+// @updateURL     https://github.com/andywang425/Bilibili-SGTH/raw/master/B%E7%AB%99%E7%9B%B4%E6%92%AD%E8%87%AA%E5%8A%A8%E6%8A%A2%E8%BE%A3%E6%9D%A1.user.js
+// @downloadUR    https://github.com/andywang425/Bilibili-SGTH/raw/master/B%E7%AB%99%E7%9B%B4%E6%92%AD%E8%87%AA%E5%8A%A8%E6%8A%A2%E8%BE%A3%E6%9D%A1.user.js
 // @homepageURL   https://github.com/andywang425/Bilibili-SGTH/
 // @supportURL    https://github.com/andywang425/Bilibili-SGTH/issues
 // @icon          https://s1.hdslb.com/bfs/live/d57afb7c5596359970eb430655c6aef501a268ab.png
