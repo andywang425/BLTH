ef660b8133ebb7104fe52ede557d7af268d2e69e
andywang425 <13120989656@163.com>
2022-01-22 10:43:37 +0800

    5.7.9.5 1. 修复无法获取粉丝勋章列表的bug。 2. 调整了【移除排行榜】功能。

--- assets/js/library/BilibiliAPI_Mod.js
+++ assets/js/library/BilibiliAPI_Mod.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          BilibiliAPI_mod
 // @namespace     https://github.com/SeaLoong
-// @version       3.0.9
+// @version       3.1
 // @description   BilibiliAPI，PC端抓包研究所得，原作者是SeaLoong。我在此基础上补充新的API。
 // @author        SeaLoong, andywang425
 // @require       https://code.jquery.com/jquery-3.6.0.min.js
