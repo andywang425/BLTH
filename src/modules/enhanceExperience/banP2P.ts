d06fd15b90342e9e81cda5e177f97c114f680330
andywang425 <13120989656@163.com>
2023-08-05 22:07:42 +0800

    🐳 chore: trigger build

--- src/modules/enhanceExperience/banP2P.ts
+++ src/modules/enhanceExperience/banP2P.ts
@@ -7,11 +7,7 @@ class BanP2P extends BaseModule {
   config = this.moduleStore.moduleConfig.EnhanceExperience.banp2p
 
   private async banP2P() {
-    const RTClist: string[] = [
-      'RTCPeerConnection',
-      'mozRTCPeerConnection',
-      'webkitRTCPeerConnection'
-    ]
+    const RTClist = ['RTCPeerConnection', 'mozRTCPeerConnection', 'webkitRTCPeerConnection']
     for (const i of RTClist) {
       // 判断属性是否存在并且是否可配置
       if (
