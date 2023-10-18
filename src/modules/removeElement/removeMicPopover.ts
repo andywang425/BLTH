645e174f62b964210a807e773086abfe2b531f4c
ADJazz <68748165+ADJazzzz@users.noreply.github.com>
2023-10-19 00:05:56 +0800

    🌈 style: format

--- src/modules/removeElement/removeMicPopover.ts
+++ src/modules/removeElement/removeMicPopover.ts
@@ -2,16 +2,16 @@ import { GM_addStyle } from '$'
 import BaseModule from '../BaseModule'
 
 class removeMicPopover extends BaseModule {
-    static runOnMultiplePages = true
+  static runOnMultiplePages = true
 
-    config = this.moduleStore.moduleConfig.RemoveElement.removeMicPopover
+  config = this.moduleStore.moduleConfig.RemoveElement.removeMicPopover
 
-    public async run() {
-        this.logger.log('移除连麦状态提示模块开始运行')
-        if (this.config.enabled) {
-            GM_addStyle('.lin-mic-cntr { display: none !important }')
-        }
+  public async run() {
+    this.logger.log('移除连麦状态提示模块开始运行')
+    if (this.config.enabled) {
+      GM_addStyle('.lin-mic-cntr { display: none !important }')
     }
+  }
 }
 
 export default removeMicPopover
