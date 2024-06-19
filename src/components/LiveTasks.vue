8d63c384fade17394907412d561127575aee4955
andywang425 <56548456+andywang425@users.noreply.github.com>
2024-06-19 21:53:20 +0800

    🐞 fix: 继续尝试修复【编辑粉丝勋章名单】弹窗相关问题 加入nextTick()

--- src/components/LiveTasks.vue
+++ src/components/LiveTasks.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, nextTick, ref, watch } from 'vue'
 import { useModuleStore } from '../stores/useModuleStore'
 import { Edit, Delete } from '@element-plus/icons-vue'
 import { ElMessage, ElMessageBox, ElTable } from 'element-plus'
@@ -83,23 +83,25 @@ let firstClickEditList = true
 /**
  * 编辑名单
  */
-const handleEditList = () => {
+const handleEditList = async () => {
   medalInfoPanelVisible.value = !medalInfoPanelVisible.value
-  // 如果没有粉丝勋章信息，先获取
+
   if (firstClickEditList) {
+    // 第一次点击编辑名单按钮时需要初始化多选框状态
+    firstClickEditList = false
+    // 等对话框加载好后再对表格进行操作
+    await nextTick()
+    // 如果没有粉丝勋章信息，先获取
     if (!biliStore.fansMedals) {
       medalInfoLoading.value = true
       // 等待数据被获取到
-      const unwatch = watch(
-        () => medalInfoTableData.value,
+      watch(
+        medalInfoTableData,
         (newData) => {
-          if (newData) {
-            unwatch()
-            firstClickEditList = false
-            initSelection(newData)
-            medalInfoLoading.value = false
-          }
-        }
+          initSelection(newData)
+          medalInfoLoading.value = false
+        },
+        { once: true }
       )
       // 利用 emitter 通知 FansMedals 模块去获取数据
       moduleStore.emitter.emit('Default_FansMedals', {
@@ -112,37 +114,21 @@ const handleEditList = () => {
 }
 /** 用来管理多选框状态的表格Ref */
 const medalInfoTableRef = ref<InstanceType<typeof ElTable>>()
-/** 是否锁住配置 */
-let lockConfig = false
+
 /** 初始化多选框选择状态 */
 const initSelection = (rows?: MedalInfoRow[]) => {
-  lockConfig = true
   if (rows) {
-    // 如果直接使用 medalInfoTableRef.value，medalInfoTableRef.value 可能为 undefined
-    const unwatch = watch(
-      () => medalInfoTableRef.value,
-      (newValue) => {
-        if (newValue) {
-          // unwatch 可能还未初始化，延迟到下一个空闲时间点执行
-          setTimeout(() => unwatch(), 0)
-          config.medalTasks.roomidList.forEach((roomid) =>
-            newValue.toggleRowSelection(
-              rows.find((row) => row.roomid === roomid),
-              true
-            )
-          )
-        }
-      },
-      { immediate: true }
+    config.medalTasks.roomidList.forEach((roomid) =>
+      medalInfoTableRef.value?.toggleRowSelection(
+        rows.find((row) => row.roomid === roomid),
+        true
+      )
     )
   }
-  lockConfig = false
 }
 
 function handleSelectionChange(selectedRows: MedalInfoRow[]) {
-  if (!lockConfig) {
-    config.medalTasks.roomidList = selectedRows.map((row) => row.roomid)
-  }
+  config.medalTasks.roomidList = selectedRows.map((row) => row.roomid)
 }
 
 function handleRowClick(row: MedalInfoRow, _column: any, event: PointerEvent) {
