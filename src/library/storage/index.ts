203f347c2606b4035ae17fb5edfceb03831ffada
andywang425 <56548456+andywang425@users.noreply.github.com>
2024-07-27 14:09:49 +0800

    🎈 perf: 优化Storage.mergeConfigs方法 - 性能提升 - 不再修改当前配置 - 泛型支持

--- src/library/storage/index.ts
+++ src/library/storage/index.ts
@@ -5,47 +5,41 @@ import _ from 'lodash'
 
 class Storage {
   /**
-   * 递归合并配置项。删除当前配置中不存在于默认配置的键，补上相对于默认配置缺少的键值，其它键值不变。
+   * 递归合并配置项。删除当前配置中不存在于默认配置的键，补上相对于默认配置缺少的键值，其它键值不变
    *
-   * 该方法会修改当前配置。
+   * 该方法不会修改当前配置
    *
-   * @param current_config_item 当前配置
-   * @param default_config_item 默认配置
+   * @param currentConfig 当前配置
+   * @param defaultConfig 默认配置
    * @returns 修改后的当前配置
    * @example
    *
    * const current_config = { enabled: true, details: { type: 'efg', status: 'ok' }, msg: 'hi' };
-   * const default_config = { enabled: false, details: { type: 'abc', num = 1 } };
+   * const default_config = { enabled: false, details: { type: 'abc', num: 1 } };
    *
-   * mergeConfig(current_config, default_config);
-   * // => { enabled: true, details: { type: 'efg', num = 1 } }
+   * mergeConfigs(current_config, default_config);
+   * // => { enabled: true, details: { type: 'efg', num: 1 } }
    */
-  private static mergeConfigs(current_config_item: any, default_config_item: any): any {
-    // 获取仅在当前配置中存在的键
-    const keysOnlyInCurrentConfigItem = _.difference(
-      _.keys(current_config_item),
-      _.keys(default_config_item)
-    )
-    // 删除当前配置中的这些键
-    current_config_item = _.omit(current_config_item, keysOnlyInCurrentConfigItem)
-    // 获取仅在默认配置中存在的键
-    const keysOnlyInDefaultItem = _.difference(
-      _.keys(default_config_item),
-      _.keys(current_config_item)
-    )
-    // 把这些键值对分配给当前配置
-    _.assign(current_config_item, _.pick(default_config_item, keysOnlyInDefaultItem))
-    // 遍历当前配置的键值对，刚分配的那些键值对肯定是正确的所以不用遍历
-    for (const [key, value] of Object.entries(current_config_item).filter(
-      (keyValue) => !keysOnlyInDefaultItem.includes(keyValue[0])
-    )) {
-      // 只递归合并普通对象（过滤 number, string, Array, null 等）
-      if (_.isPlainObject(value)) {
-        current_config_item[key] = this.mergeConfigs(value, default_config_item[key])
-      }
-    }
+  private static mergeConfigs<T extends Record<string, any>>(
+    currentConfig: Record<string, any>,
+    defaultConfig: T
+  ): T {
+    // 取出当前配置中存在于默认配置的键以及其所对应的值
+    const config = _.pick(currentConfig, _.keys(defaultConfig))
+    // 补上缺失的键值对
+    _.defaults(config, defaultConfig)
 
-    return current_config_item
+    _.forOwn(config, (value, key, object) => {
+      if (_.isPlainObject(value) && _.isPlainObject(defaultConfig[key])) {
+        // 如果都是普通对象，递归合并子配置项
+        object[key] = this.mergeConfigs(value, defaultConfig[key])
+      } else {
+        // 否则直接赋值
+        object[key] = defaultConfig[key]
+      }
+    })
+
+    return config as T
   }
 
   public static setUiConfig(uiConfig: UiConfig) {
