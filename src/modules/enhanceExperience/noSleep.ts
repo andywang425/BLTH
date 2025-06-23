bf7bd5c239bedad83d3d2af732133bc8a5987962
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-06-23 14:59:47 +0800

    ✨ feat: 屏蔽挂机检测强化，规避页面可见性相关检测

--- src/modules/enhanceExperience/noSleep.ts
+++ src/modules/enhanceExperience/noSleep.ts
@@ -1,19 +1,30 @@
-import type { RunAtMoment } from '@/types'
+import type { OnFrameTypes, RunAtMoment } from '@/types'
 import BaseModule from '../BaseModule'
 
 class NoSleep extends BaseModule {
   static runOnMultiplePages: boolean = true
   static runAt: RunAtMoment = 'window-load'
+  static onFrame: OnFrameTypes = 'top'
   static runAfterDefault: boolean = false
 
   config = this.moduleStore.moduleConfig.EnhanceExperience.noSleep
 
   public run(): void {
     this.logger.log('屏蔽挂机检测模块开始运行')
-    // 每5分钟触发一次 MouseEvent
+    // 每分钟触发一次 MouseEvent
     setInterval(() => {
       document.dispatchEvent(new MouseEvent('mousemove'))
-    }, 3e5)
+    }, 60e3)
+
+    // 修改页面可见性相关属性
+    try {
+      Object.defineProperties(document, {
+        visibilityState: { value: 'visible' },
+        hidden: { value: false },
+      })
+    } catch (e) {
+      this.logger.warn('修改页面可见性相关属性失败，可能已被其它脚本锁定', e)
+    }
   }
 }
 
