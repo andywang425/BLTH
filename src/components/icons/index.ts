00f0794e04247d917bdd00da29312ff21e25b02d
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-08-02 18:07:57 +0800

    🎈 perf

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,4 @@
-// 此处导出的名称不能和 @element-plus/icons-vue 中的图标名称重复
+// 此处导出的名称不能和 @element-plus/icons-vue 导出的图标名称重复，因为它们都会被注册为全局组件
 export { default as Tasks } from './TasksIcon.vue'
 export { default as Info } from './InfoIcon.vue'
 export { default as TaskStatus } from './TaskStatusIcon.vue'
--- src/types/vue.d.ts
+++ src/types/vue.d.ts
@@ -1,7 +1,7 @@
 import { Info, Tasks, TaskStatus } from '@/components/icons'
 
 declare module 'vue' {
-  interface GlobalComponents {
+  export interface GlobalComponents {
     Info: typeof Info
     Tasks: typeof Tasks
     TaskStatus: typeof TaskStatus
