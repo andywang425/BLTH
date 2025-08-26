2bd6445376296e650f4260973ee94e6dc9cc9b01
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-08-26 23:30:25 +0800

    ✨ feat(usePlayerStore): 整合配置为config；新配置参数

--- src/stores/usePlayerStore.ts
+++ src/stores/usePlayerStore.ts
@@ -1,7 +1,37 @@
 import { unsafeWindow } from '$'
 import { defineStore } from 'pinia'
 import { waitForMoment } from '@/library/utils'
-import { readonly, ref } from 'vue'
+import { ref } from 'vue'
+import _ from 'lodash'
+
+export interface PlayerStoreConfig {
+  getPlayer: {
+    /** 检查间隔（毫秒），默认 200 毫秒 */
+    interval?: number
+    /** 超时时间（毫秒），默认 10 秒 */
+    timeout?: number
+  }
+  waitForLiveStatus: {
+    /** 检查间隔（毫秒），默认 10 秒 */
+    interval?: number
+    /** 超时时间（毫秒），默认不会超时 */
+    timeout?: number
+    /** 立刻成功回调 */
+    onImmediateSuccess?: () => void
+    /** 需要等待回调 */
+    onNeedWait?: () => void
+  }
+}
+
+const DEFAULT_CONFIG: PlayerStoreConfig = {
+  getPlayer: {
+    interval: 200,
+    timeout: 12e3,
+  },
+  waitForLiveStatus: {
+    interval: 10e3,
+  },
+}
 
 export const usePlayerStore = defineStore('player', () => {
   // 播放器实例（window.livePlayer）
@@ -9,13 +39,13 @@ export const usePlayerStore = defineStore('player', () => {
 
   /**
    * 获取播放器实例
-   * @param interval 检查间隔（毫秒），默认 200 毫秒
-   * @param timeout 超时时间（毫秒），默认 10 秒
+   * @param config 配置
    */
   const getPlayer = async (
-    interval: number = 200,
-    timeout: number = 12e3,
+    config: PlayerStoreConfig['getPlayer'] = DEFAULT_CONFIG.getPlayer,
   ): Promise<Window['livePlayer']> => {
+    _.defaults(config, DEFAULT_CONFIG.getPlayer)
+
     if (player.value) {
       return player.value
     }
@@ -39,12 +69,12 @@ export const usePlayerStore = defineStore('player', () => {
           player.value = unsafeWindow.livePlayer
           resolve(player.value)
         }
-      }, interval)
+      }, config.interval)
 
       const timeoutTimer = setTimeout(() => {
         clearInterval(findPlayerTimer)
         reject('等待播放器超时')
-      }, timeout)
+      }, config.timeout)
     })
   }
 
@@ -52,22 +82,25 @@ export const usePlayerStore = defineStore('player', () => {
    * 等待直播间开播或结束
    *
    * @param status 直播状态：0 未开播，1 开播
-   * @param interval 检查间隔（毫秒），默认 10 秒
-   * @param timeout 超时时间（毫秒），默认不会超时
+   * @param config 配置
    * @returns 是否在超时前达到目标状态
    */
   const waitForLiveStatus = async (
     status: 0 | 1,
-    interval: number = 10e3,
-    timeout?: number,
+    config: PlayerStoreConfig['waitForLiveStatus'] = DEFAULT_CONFIG.waitForLiveStatus,
   ): Promise<boolean> => {
+    _.defaults(config, DEFAULT_CONFIG.waitForLiveStatus)
+
     const player = await getPlayer()
 
     const liveStatus = player.getPlayerInfo().liveStatus
 
     if (liveStatus === status) {
+      config.onImmediateSuccess?.()
       return true
     }
+
+    config.onNeedWait?.()
 
     return new Promise<boolean>((resolve) => {
       const liveStatusTimer = setInterval(() => {
@@ -77,19 +110,19 @@ export const usePlayerStore = defineStore('player', () => {
           clearInterval(liveStatusTimer)
           resolve(true)
         }
-      }, interval)
+      }, config.interval)
 
       const timeoutTimer =
-        timeout &&
+        config.timeout &&
         setTimeout(() => {
           clearInterval(liveStatusTimer)
           resolve(false)
-        }, timeout)
+        }, config.timeout)
     })
   }
 
   return {
-    player: readonly(player),
+    player,
     getPlayer,
     waitForLiveStatus,
   }
