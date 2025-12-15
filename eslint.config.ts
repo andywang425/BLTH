b245443d104d66ebd8c090016d85bc16324a954f
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-12-15 15:11:49 +0800

    🐳 chore(eslint.config.ts): 改为官方推荐用法

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,14 +3,14 @@ import { includeIgnoreFile } from '@eslint/compat'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
 // configureVueProject({ scriptLangs: ['ts', 'tsx'] })
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
-const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
+const gitignorePath = path.resolve(import.meta.dirname, '.gitignore')
 
 export default defineConfigWithVueTs(
   {
