4dd0cddb84ae185a9e8c8e1322234d9e5fbbb971
andywang425 <56548456+andywang425@users.noreply.github.com>
2026-06-09 15:52:30 +0800

    🔧 build: 更新依赖和eslint配置

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,4 @@
-import { globalIgnores } from 'eslint/config'
-import { includeIgnoreFile } from '@eslint/compat'
+import { globalIgnores, includeIgnoreFile } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from 'eslint-config-prettier/flat'
